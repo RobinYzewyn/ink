08831001968d1bfe727220030417844e0c72b490
RobinYzewyn <robin.yzewyn@student.howest.be>
2021-02-24 09:37:53 +0100

    Counter

--- ui.js
+++ ui.js
@@ -1,11 +1,23 @@
 'use strict';
 const React = require('react');
 const {Text} = require('ink');
+const { useState, useEffect } = require('react');
 
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-);
+
+const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCounter(previousCounter => previousCounter + 1);
+		}, 100);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
+	return <Text color="green">{counter} tests passed</Text>;
+};
 
 module.exports = App;
